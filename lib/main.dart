cf3bbf3ffe1fddef4d2f14643896952e922cd8d7
Gabriel Victor <gabrielvgx@gmail.com>
2020-11-27 10:09:24 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:alcool_gasolina/pages/home.page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alcool Ou Gasolina',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
